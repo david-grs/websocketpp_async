255bc8c4bf8aab1f23efda8c9627197e7e2e2e5d
David G <david.a.grs@gmail.com>
2016-08-20 23:42:23 +0200

    get rid of additional thread and doing stuff in async mode only

--- main.cpp
+++ main.cpp
@@ -15,49 +15,52 @@ struct WebSocketServer
 
     std::vector<websocketpp::connection_hdl> m_connections;
 
-    void OnMessage(websocketpp::connection_hdl hdl, message_ptr msg)
-    {
-        std::cout << "on_message called with hdl: " << hdl.lock().get() << " and message: " << msg->get_payload()
-                  << std::endl;
-
-        m_connections.emplace_back(std::move(hdl));
-    }
-
-    void Run()
-    {
-        mThread->join();
-    }
-
-    void Send()
-    {
-        if (m_connections.empty())
-            return;
-        auto& hdl = m_connections.back();
-        m_connections.pop_back();
-
-        mServer.send(hdl, "{\"fx_underlyings\": [], \"otc_underlyings\": []}", websocketpp::frame::opcode::text);
-        mServer.close(hdl, 0, "bye");
-    }
-
     void Listen(int port)
     {
         using websocketpp::lib::placeholders::_1;
         using websocketpp::lib::placeholders::_2;
         using websocketpp::lib::bind;
 
-        mThread.reset(new std::thread([&]()
-                                      {
-                                          mServer.init_asio();
-                                          mServer.set_message_handler(bind(&WebSocketServer::OnMessage, this, _1, _2));
-                                          mServer.listen(websocketpp::lib::asio::ip::tcp::v4(), port);
-                                          mServer.start_accept();
-                                          mServer.poll_one();
-                                      }));
+        mServer.init_asio();
+        mServer.set_message_handler(bind(&WebSocketServer::OnMessage, this, _1, _2));
+
+        websocketpp::lib::error_code err;
+        mServer.listen(websocketpp::lib::asio::ip::tcp::v4(), port, err);
+
+        if (err)
+            throw std::runtime_error(err.message());
+
+        mServer.start_accept();
+        mServer.poll();
+    }
+
+    void Send()
+    {
+        if (m_connections.empty())
+            return;
+
+        auto& hdl = m_connections.back();
+        m_connections.pop_back();
+
+        mServer.send(hdl, "{\"fx_underlyings\": [\"FOO\", \"BAR\"], \"otc_underlyings\": []}", websocketpp::frame::opcode::text);
+        mServer.close(hdl, 0, "bye");
+    }
+
+    void Poll()
+    {
+        std::cout << "start polling..." << std::endl;
+        auto sz = mServer.poll();
+        std::cout << "poll done, " << sz << " handlers processed" << std::endl;
+    }
+
+    void OnMessage(websocketpp::connection_hdl hdl, message_ptr msg)
+    {
+        std::cout << "on_message called with hdl: " << hdl.lock().get() << " and message: " << msg->get_payload() << std::endl;
+        m_connections.emplace_back(hdl);
     }
 
 private:
     server mServer;
-    std::unique_ptr<std::thread> mThread;
 };
 
 int main(int argc, char** argv)
@@ -70,12 +73,13 @@ int main(int argc, char** argv)
     WebSocketServer server;
     server.Listen(port);
 
+    // simulating a busy event loop that polls
     while (1)
     {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        server.Poll();
         server.Send();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
-    server.Run();
     return 0;
 }
