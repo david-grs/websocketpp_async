cec41d4a81508c3fca6d6b99de285f66670f25d4
David G <david.a.grs@gmail.com>
2016-08-20 23:20:19 +0200

    fix indent

--- main.cpp
+++ main.cpp
@@ -1,83 +1,78 @@
-#include <iostream>
-#include <thread>
-#include <memory>
-
-#include <cstdlib>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
 #include <vector>
 #include <chrono>
+#include <iostream>
+#include <thread>
+#include <memory>
+#include <cstdlib>
+
 struct Server
 {
-  using server = websocketpp::server<websocketpp::config::asio>;
-  using message_ptr = server::message_ptr;
+    using server = websocketpp::server<websocketpp::config::asio>;
+    using message_ptr = server::message_ptr;
 
-  std::vector<websocketpp::connection_hdl> m_connections;
+    std::vector<websocketpp::connection_hdl> m_connections;
 
-  void OnMessage(websocketpp::connection_hdl hdl, message_ptr msg)
-  {
-    std::cout << "on_message called with hdl: " << hdl.lock().get()
-              << " and message: " << msg->get_payload()
-              << std::endl;
+    void OnMessage(websocketpp::connection_hdl hdl, message_ptr msg)
+    {
+        std::cout << "on_message called with hdl: " << hdl.lock().get() << " and message: " << msg->get_payload()
+                  << std::endl;
 
-    m_connections.emplace_back(std::move(hdl));
-  }
+        m_connections.emplace_back(std::move(hdl));
+    }
 
-  void Run()
-  {
-    mThread->join();
-  }
+    void Run()
+    {
+        mThread->join();
+    }
 
-  void Send()
-  {
-      if (m_connections.empty())
-          return;
-      auto& hdl = m_connections.back();
-      m_connections.pop_back();
+    void Send()
+    {
+        if (m_connections.empty())
+            return;
+        auto& hdl = m_connections.back();
+        m_connections.pop_back();
 
-       mServer.send(hdl, "{\"fx_underlyings\": [], \"otc_underlyings\": []}", websocketpp::frame::opcode::text);
-mServer.close(hdl, 0, "bye");
-  }
+        mServer.send(hdl, "{\"fx_underlyings\": [], \"otc_underlyings\": []}", websocketpp::frame::opcode::text);
+        mServer.close(hdl, 0, "bye");
+    }
 
-  void Listen(int port)
-  {
-    using websocketpp::lib::placeholders::_1;
-    using websocketpp::lib::placeholders::_2;
-    using websocketpp::lib::bind;
+    void Listen(int port)
+    {
+        using websocketpp::lib::placeholders::_1;
+        using websocketpp::lib::placeholders::_2;
+        using websocketpp::lib::bind;
 
-    mThread.reset(new std::thread([&]()
-      {
-        mServer.init_asio();
-        mServer.set_message_handler(bind(&Server::OnMessage, this, _1, _2));
-        mServer.listen(websocketpp::lib::asio::ip::tcp::v4(), port);
-        mServer.start_accept();
+        mThread.reset(new std::thread([&]()
+                                      {
+                                          mServer.init_asio();
+                                          mServer.set_message_handler(bind(&Server::OnMessage, this, _1, _2));
+                                          mServer.listen(websocketpp::lib::asio::ip::tcp::v4(), port);
+                                          mServer.start_accept();
 
-        mServer.poll_one();
-        std::cout << "bla" << std::endl;
-    }));
-  }
+                                          mServer.poll_one();
+                                          std::cout << "bla" << std::endl;
+                                      }));
+    }
 
-
-private:
-  server mServer;
-  std::unique_ptr<std::thread> mThread;
+   private:
+    server mServer;
+    std::unique_ptr<std::thread> mThread;
 };
 
+int main(int argc, char** argv)
+{
+    Server serv;
+    serv.Listen(atoi(argv[1]));
+    std::cout << "run" << std::endl;
 
-
-// Define a callback to handle incoming messages
-
-int main(int argc, char **argv) {
-  Server serv;
-   serv.Listen(atoi(argv[1]));
-   std::cout << "run" << std::endl;
-
-   while (1)
-   {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    serv.Send();
-   }
-   serv.Run();
- return 0;
+    while (1)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        serv.Send();
+    }
+    serv.Run();
+    return 0;
 }
